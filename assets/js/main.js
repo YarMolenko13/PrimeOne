d7a4f6002d9025ebb941df0c7dfb0709a7ce5970
Yaroslav Molodcov <blistacelniy@yahoo.com>
2020-10-21 21:47:22 +0300

    did adapt menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
+;!(function ($) {
+    $.fn.classes = function (callback) {
+        var classes = [];
+        $.each(this, function (i, v) {
+            var splitClassName = v.className.split(/\s+/);
+            for (var j = 0; j < splitClassName.length; j++) {
+                var className = splitClassName[j];
+                if (-1 === classes.indexOf(className)) {
+                    classes.push(className);
+                }
+            }
+        });
+        if ('function' === typeof callback) {
+            for (var i in classes) {
+                callback(classes[i]);
+            }
+        }
+        return classes;
+    };
+})(jQuery);
+
 $(document).ready(function() {
 	function ibg(){
 		$.each($('.ibg'), function(index, val) {
@@ -8,4 +29,9 @@ $(document).ready(function() {
 	}
 	
 	ibg();
+
+	let burgBtn = $('.burg-btn')
+	burgBtn.on('touchstart', function(event) {
+		$('.menu-icon, .menu-body, .header-bottom-menu-list').toggleClass('active')
+	});
 })
